269a7934a04c4b87b657163f78bf806b0999ca3e
Tommaso Prandin <tommaso.prandin@studenti.unipd.it>
2025-07-06 19:37:12 +0200

    RTIC simple demo on QEMU

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,63 @@
 #![no_main]
 
 use cortex_m as _;
-use cortex_m_semihosting::debug;
 use defmt_semihosting as _;
 use panic_halt as _;
 
-use cortex_m_rt::entry;
+use stm32f4xx_hal as _;
 
-#[entry]
-fn main() -> ! {
-    for i in 0..10 {
-        defmt::info!("Hello, world! {}", i);
+#[rtic::app(
+    device = stm32f4xx_hal::pac,
+    dispatchers = [EXTI0, EXTI1]
+)]
+mod app {
+
+    use cortex_m_semihosting::debug;
+
+    // Shared resources go here
+    #[shared]
+    struct Shared {}
+
+    // Local resources go here
+    #[local]
+    struct Local {}
+
+    #[init]
+    fn init(_: init::Context) -> (Shared, Local) {
+        defmt::info!("Init");
+
+        // TODO setup monotonic if used
+        // let sysclk = { /* clock setup + returning sysclk as an u32 */ };
+        // let token = rtic_monotonics::create_systick_token!();
+        // rtic_monotonics::systick::Systick::new(cx.core.SYST, sysclk, token);
+
+        task1::spawn().ok();
+
+        (
+            Shared {
+                // Initialization of shared resources go here
+            },
+            Local {
+                // Initialization of local resources go here
+            },
+        )
     }
-    debug::exit(debug::EXIT_SUCCESS);
-    loop {}
+
+    // Optional idle, can be removed if not needed.
+    #[idle]
+    fn idle(_cx: idle::Context) -> ! {
+        defmt::info!("Idle alive");
+        for i in 0..10 {
+            defmt::info!("Idle loop tick {}", i);
+        }
+        defmt::info!("Goodbye");
+        debug::exit(debug::EXIT_SUCCESS);
+        loop {}
+    }
+
+    // TODO: Add tasks
+    #[task(priority = 1)]
+    async fn task1(_cx: task1::Context) {
+        defmt::info!("Hello from task1!");
+    }
 }
