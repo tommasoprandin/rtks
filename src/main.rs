a4b0969d0ff0a91ada76aae974dd0f248759c2ad
Tommaso Prandin <tommaso.prandin@raceup.it>
2025-07-27 21:53:38 +0200

    Updated main with time, event server, activation log reader and demo producer

--- src/main.rs
+++ src/main.rs
@@ -1,105 +1,145 @@
 #![no_std]
 #![no_main]
 
-use cortex_m as _;
-use defmt_semihosting as _;
-use panic_halt as _;
+mod auxiliary;
+mod resources;
+mod tasks;
+mod time;
+mod workload;
 
+use cortex_m::interrupt;
+use cortex_m_semihosting::debug::{self, EXIT_FAILURE};
+use defmt_semihosting as _;
 use stm32f4xx_hal as _;
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    interrupt::disable();
+
+    defmt::error!("Panic: {}", info);
+    debug::exit(EXIT_FAILURE);
+
+    loop {}
+}
 
 #[rtic::app(
     device = stm32f4xx_hal::pac,
-    dispatchers = [EXTI0, EXTI1]
+    dispatchers = [EXTI0, EXTI1, EXTI2]
 )]
 mod app {
 
-    use cortex_m_semihosting::debug;
-    use rtic_sync::channel::{Receiver, Sender};
+    use crate::{
+        resources::{
+            activation_log::ActivationLog,
+            event_queue::{EventQueue, EventQueueSignaler, EventQueueWaiter},
+            task_semaphore::{TaskSemaphore, TaskSemaphoreSignaler, TaskSemaphoreWaiter},
+        },
+        tasks,
+        time::Mono,
+    };
+    use cortex_m::asm::nop;
+    use rtic_monotonics::{fugit::RateExtU32 as _, systick::prelude::*};
+    use stm32f4xx_hal::rcc::RccExt;
 
-    const CAPACITY: usize = 5;
     // Shared resources go here
     #[shared]
     struct Shared {
-        val: u32,
+        activation_log: ActivationLog,
     }
 
     // Local resources go here
     #[local]
     struct Local {
-        s: Sender<'static, u32, CAPACITY>,
-        r: Receiver<'static, u32, CAPACITY>,
+        event_signaler: EventQueueSignaler<'static>,
+        event_waiter: EventQueueWaiter<'static>,
+        activation_log_reader_signaler: TaskSemaphoreSignaler<'static>,
+        activation_log_reader_waiter: TaskSemaphoreWaiter<'static>,
     }
 
+    // Timer struct
     #[init]
-    fn init(_: init::Context) -> (Shared, Local) {
+    fn init(ctx: init::Context) -> (Shared, Local) {
         defmt::info!("Init");
 
-        // TODO setup monotonic if used
-        // let sysclk = { /* clock setup + returning sysclk as an u32 */ };
-        // let token = rtic_monotonics::create_systick_token!();
-        // rtic_monotonics::systick::Systick::new(cx.core.SYST, sysclk, token);
+        // Extract device from context
+        let peripherals = ctx.device;
+        let core = ctx.core;
 
-        let (s, r) = rtic_sync::make_channel!(u32, CAPACITY);
+        // Clocks setup
+        let rcc = peripherals.RCC.constrain();
+        let clocks = rcc
+            .cfgr
+            .use_hse(8.MHz())
+            .sysclk(168.MHz())
+            .pclk1(42.MHz())
+            .freeze();
 
-        task1::spawn().ok();
-        task2::spawn().ok();
+        defmt::info!("Clocks initialized");
+
+        // Setup monotonic timer
+        Mono::start(core.SYST, clocks.sysclk().to_Hz());
+
+        // Setup event queue
+        let (event_waiter, event_signaler) = EventQueue::new();
+        // Setup activation log
+        let activation_log = ActivationLog::new();
+        // Setup activation log reader semaphore
+        let (activation_log_reader_waiter, activation_log_reader_signaler) = TaskSemaphore::new();
+
+        external_event_server::spawn().expect("Error spawning external event server");
+        producer_task::spawn().expect("Error spawning producer task");
+        activation_log_reader::spawn().expect("Error spawning activatio log reader task");
 
         (
             Shared {
                 // Initialization of shared resources go here
-                val: 0,
+                activation_log,
             },
             Local {
                 // Initialization of local resources go here
-                s,
-                r,
+                event_signaler,
+                event_waiter,
+                activation_log_reader_signaler,
+                activation_log_reader_waiter,
             },
         )
     }
 
-    // Optional idle, can be removed if not needed.
     #[idle]
-    fn idle(_cx: idle::Context) -> ! {
-        defmt::info!("Idling...");
-        defmt::info!("Goodbye");
-        debug::exit(debug::EXIT_SUCCESS);
-        loop {}
+    fn idle(_: idle::Context) -> ! {
+        loop {
+            nop();
+        }
     }
 
-    #[task(priority = 1, local=[s], shared = [val])]
-    async fn task1(cx: task1::Context) {
-        let mut val = cx.shared.val;
-        let s = cx.local.s;
-
-        defmt::info!("Hello from task1!");
-
-        val.lock(|v| {
-            *v = 1;
-            defmt::info!("Shared value is now: {}", v);
-        });
-        
-
-        match s.send(42).await {
-            Ok(_) => defmt::info!("Sent value 42"),
-            Err(e) => defmt::error!("Failed to send value: {}", e),
-        }; 
+    #[task(priority = 11, local=[event_waiter], shared=[activation_log])]
+    async fn external_event_server(mut cx: external_event_server::Context) -> ! {
+        tasks::external_event_server::external_event_server(
+            cx.local.event_waiter,
+            &mut cx.shared.activation_log,
+        )
+        .await;
     }
 
-    #[task(priority = 2, local = [r], shared = [val])]
-    async fn task2(cx: task2::Context) {
-        let mut val = cx.shared.val;
-        let r = cx.local.r;
+    #[task(priority = 3, local=[activation_log_reader_waiter], shared=[activation_log])]
+    async fn activation_log_reader(mut cx: activation_log_reader::Context) -> ! {
+        tasks::activation_log_reader::activation_log_reader(
+            cx.local.activation_log_reader_waiter,
+            &mut cx.shared.activation_log,
+        ).await;
+    }
 
-        defmt::info!("Hello from task2!");
-
-        val.lock(|v| {
-            *v = 2;
-            defmt::info!("Shared value is now: {}", v);
-        });
-
-        match r.recv().await {
-            Ok(value) => defmt::info!("Received value: {}", value),
-            Err(e) => defmt::error!("Failed to receive value: {}", e),
-        };
+    #[task(priority = 1, local=[event_signaler, activation_log_reader_signaler])]
+    async fn producer_task(cx: producer_task::Context) -> ! {
+        let events = cx.local.event_signaler;
+        let al_semaphore = cx.local.activation_log_reader_signaler;
+        loop {
+            for _ in 1..=3 {
+                Mono::delay(500.millis()).await;
+                events.signal(());
+            }
+            Mono::delay(1_000.millis()).await;
+            al_semaphore.signal();
+        }
     }
 }
