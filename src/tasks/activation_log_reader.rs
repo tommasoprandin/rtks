4ab8f8678984a5991c1500eded9aa0914a0e84bc
Tommaso Prandin <tommaso.prandin@raceup.it>
2025-07-27 21:52:43 +0200

    Implemented activation log and activation log reader task

---
+++ src/tasks/activation_log_reader.rs
@@ -0,0 +1,30 @@
+use crate::{
+    resources::{
+        activation_log::ActivationLog,
+        task_semaphore::TaskSemaphoreWaiter,
+    },
+    workload,
+};
+
+pub async fn activation_log_reader(
+    semaphore: &mut TaskSemaphoreWaiter<'_>,
+    activation_log: &mut impl rtic::Mutex<T = ActivationLog>,
+) -> ! {
+    loop {
+        semaphore.wait().await;
+        if let Err(err) = workload::small_whetstone(1_000) {
+            defmt::error!(
+                "Error computing whetstone in activation log reader: {}",
+                err
+            );
+        }
+        activation_log.lock(|al| {
+            let (activations, last) = al.read();
+            defmt::info!(
+                "Activation log reader: activations = {}, last = {}",
+                activations,
+                last
+            );
+        })
+    }
+}
